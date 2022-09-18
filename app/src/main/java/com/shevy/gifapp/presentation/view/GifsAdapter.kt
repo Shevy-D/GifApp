8ce1ece34a65e15d289df38dd2b5c85dea3cca49
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-09-18 17:14:49 +0500

    Added click listener by higher-order functions. Made refactoring

--- app/src/main/java/com/shevy/gifapp/presentation/view/GifsAdapter.kt
+++ app/src/main/java/com/shevy/gifapp/presentation/view/GifsAdapter.kt
@@ -1,63 +1,52 @@
 package com.shevy.gifapp.presentation.view
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.shevy.gifapp.R
-import com.shevy.gifapp.data.Gifs
 import com.shevy.gifapp.databinding.RecyclerviewItemBinding
 import com.shevy.gifapp.domain.interactors.Gif
 
 class GifsAdapter(
-    private val gifs: List<Gifs>
-    //private val listener: OnItemClickListener,
-    //added it
-    //private val onGifSelected: (gif: Gif) -> Unit
+    private val onGifSelected: ((gif: Gif) -> Unit)
 ) :
     RecyclerView.Adapter<GifsAdapter.GifsViewHolder>() {
 
-    //added it
-/*    private val gifs = mutableListOf<Gif>()
+    private val gif = mutableListOf<Gif>()
 
     @SuppressLint("NotifyDataSetChanged")
-    fun setGifs(gifs: List<Gif>) {
-        this.gifs.apply {
+    fun setGifs(gif: List<Gif>) {
+        this.gif.apply {
             clear()
-            addAll(gifs)
+            addAll(gif)
         }
         notifyDataSetChanged()
-    }*/
-
-
-    interface OnItemClickListener {
-        fun onItemClick(position: Int)
     }
 
-    inner class GifsViewHolder(item: View) :
+    inner class GifsViewHolder(item: View, private val onGifSelected: (gif: Gif) -> Unit) :
         RecyclerView.ViewHolder(item) {
         private val binding = RecyclerviewItemBinding.bind(item)
         val imageView = binding.imageView
-/*        init {
-            item.setOnClickListener {
-                listener.onItemClick(adapterPosition)
-            }
-        }*/
+
+        init {
+            itemView.setOnClickListener { onGifSelected(gif[adapterPosition]) }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GifsViewHolder {
         return GifsViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item, parent, false),
+            onGifSelected
         )
     }
 
     override fun onBindViewHolder(holder: GifsViewHolder, position: Int) {
-        val itemsGifs = gifs[position]
-        Glide.with(holder.imageView.context).load(itemsGifs.images.downsized.url).into(holder.imageView)
+        val itemsGifs = gif[position]
+        Glide.with(holder.imageView.context).load(itemsGifs.previewUrl).into(holder.imageView)
     }
 
-    override fun getItemCount(): Int = gifs.size
+    override fun getItemCount(): Int = gif.size
 }
--- app/src/main/java/com/shevy/gifapp/presentation/view/MainActivity.kt
+++ app/src/main/java/com/shevy/gifapp/presentation/view/MainActivity.kt
@@ -2,37 +2,30 @@ package com.shevy.gifapp.presentation.view
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.shevy.gifapp.data.GiphyDC
 import com.shevy.gifapp.databinding.ActivityMainBinding
-import com.shevy.gifapp.presentation.model.GifsApi
 import com.shevy.gifapp.domain.interactors.Gif
 import com.shevy.gifapp.domain.interactors.GifsInteractor
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    lateinit var apiResponse: Call<GiphyDC>
     lateinit var searchEditText: String
     private val interactor = GifsInteractor.create()
 
-    //private val viewModel by ViewModelProvider(this).get()
-    private lateinit var adapter: GifsAdapter
-
-/*    // TODO инициировать адаптер сразу здесть
-    private val adapter = ListenerSample(::onClick)
-    //adapter.addGifs(gifs)*/
+    // TODO инициировать адаптер сразу здесть
+    //private val adapter = ListenerSample(::onClick)
+    private val adapter = GifsAdapter(::onClick)
+    //adapter.addGifs(gifs)
 
     private fun onClick(gif: Gif) {
-
+        val intent = Intent(this@MainActivity, SecondActivity::class.java)
+        intent.putExtra("url", gif.url)
+        startActivity(intent)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,22 +33,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //adapter = GifsAdapter(this)
-
         binding.recyclerView.layoutManager =
             StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        binding.recyclerView.adapter = adapter
 
         checkSearchEditText(savedInstanceState)
-
-        Log.d("TestLogs", "Search text = ${searchEditText.isEmpty()}")
-
-        apiResponse = getApiResponse()
 
         binding.searchButton.setOnClickListener {
             searchEditText = binding.searchEditText.text?.trim().toString()
 
-            apiResponse = getApiResponse()
-            apiEnqueue(apiResponse)
+            lifecycleScope.launch {
+                val gifs = interactor.getSearchingGifs(searchEditText).await()
+                adapter.setGifs(gifs)
+            }
 
             val imm: InputMethodManager =
                 getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
@@ -63,88 +53,30 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-/*        lifecycleScope.launch {
-            val gifs = interactor.getTrendingGifs().await()
-            // TODO положить гифки в адаптер
-        }*/
-
         lifecycleScope.launch {
-            //GifsInteractor.create().getTrendingGifs().await()
-            apiEnqueue(apiResponse)
+            val gifs = getApiResponse()
+            // TODO положить гифки в адаптер
+            adapter.setGifs(gifs)
         }
-        //apiEnqueue(apiResponse)
     }
 
     private fun checkSearchEditText(savedInstanceState: Bundle?) {
         searchEditText = binding.searchEditText.text?.trim().toString()
         if (savedInstanceState != null) {
             searchEditText = savedInstanceState.getString("search").toString()
-            Log.d("TestLogs", "SearchEditText = $searchEditText")
         }
     }
 
-    @JvmName("getApiResponse1")
-    private fun getApiResponse(): Call<GiphyDC> {
-        apiResponse = if (searchEditText.isEmpty()) {
-            GifsApi.create().trendingGifs("Wh80AKplXriFbdAoHIjQa6pQgEWuVwLx", 20, "g")
+    private suspend fun getApiResponse(): List<Gif> {
+        return if (searchEditText.isEmpty()) {
+            interactor.getTrendingGifs().await()
         } else {
-            GifsApi.create().searchingGifs(apiKey, searchEditText, limit, offset, rating, lang)
+            interactor.getSearchingGifs(searchEditText).await()
         }
-        return apiResponse
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString("search", binding.searchEditText.text.toString())
-    }
-
-/*    private fun createFilter(searchText: String): HashMap<String, String> {
-        val filter = HashMap<String, String>()
-        filter["api_key"] = "Wh80AKplXriFbdAoHIjQa6pQgEWuVwLx"
-        filter["q"] = searchText
-        filter["limit"] = "20"
-        filter["offset"] = "0"
-        filter["rating"] = "g"
-        filter["lang"] = "en"
-
-        return filter
-    }*/
-
-    private fun apiEnqueue(apiResponse: Call<GiphyDC>) {
-        apiResponse.enqueue(object : Callback<GiphyDC> {
-            override fun onResponse(call: Call<GiphyDC>, response: Response<GiphyDC>) {
-
-                val listener = object : GifsAdapter.OnItemClickListener {
-                    override fun onItemClick(position: Int) {
-                        val intent = Intent(this@MainActivity, SecondActivity::class.java)
-                        intent.putExtra(
-                            "url",
-                            response.body()!!.data[position].images.original.url
-                        )
-                        startActivity(intent)
-                    }
-                }
-                val recyclerAdapter =
-                    response.body()?.let {
-                        GifsAdapter(it.data /*listener*/)
-                    }
-                binding.recyclerView.adapter = recyclerAdapter
-
-                Log.d("TestLog", "On Response Success ${response.body()?.data}")
-            }
-
-            override fun onFailure(call: Call<GiphyDC>, t: Throwable) {
-                Log.d("TestLogs", "On Failure  ${t.message}")
-            }
-        })
-    }
-
-    //delete it
-    companion object {
-        private const val apiKey = "Wh80AKplXriFbdAoHIjQa6pQgEWuVwLx"
-        private const val limit = 20
-        private const val offset = "0"
-        private const val rating = "g"
-        private const val lang = "en"
     }
 }
