fcf5fbae2fae7148cf7251baa5e992aa7a215e67
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-10-24 20:58:08 +0500

    Refactoring tests

--- app/src/test/java/com/shevy/gifapp/presentation/main/MainViewModelTest.kt
+++ app/src/test/java/com/shevy/gifapp/presentation/main/MainViewModelTest.kt
@@ -1,31 +1,76 @@
 package com.shevy.gifapp.presentation.main
 
+import com.shevy.gifapp.data.GifsInteractorImpl
 import com.shevy.gifapp.domain.interactors.Gif
-import com.shevy.gifapp.domain.interactors.GifInteractor
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.mockito.kotlin.mock
 
 class MainViewModelTest {
+/*
+    @AfterEach
+    fun afterEach() {
+        Mockito.reset(interactorTest)
+        //ArchTaskExecutor.getInstance().setDelegate(null)
+    }
+
+    @BeforeEach
+    fun beforeEach() {
+      ArchTaskExecutor.getInstance().setDelegate(object : TaskExecutor() {
+            override fun executeOnDiskIO(runnable: Runnable) {
+                runnable.run()
+            }
+
+            override fun postToMainThread(runnable: Runnable) {
+                runnable.run()
+            }
+
+            override fun isMainThread(): Boolean {
+                return true
+            }
+        })
+    }*/
+
+    @BeforeEach
+    fun setup() {
+        Dispatchers.setMain(Dispatchers.Unconfined)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
 
     @Test
-    suspend fun `Should return correct data`() {
+    fun getSearchText() = runBlocking {
+        // setup
+        val interactorTest = mock<GifsInteractorImpl>()
 
-        val interactorTest = mock<GifInteractor>()
+        val testData = listOf(
+            Gif("test preview url", "test url"),
+            Gif("test preview url2", "test url2")
+        )
         val text = "Test"
+        val result = mock<Deferred<List<Gif>>>()
+        `when`(result.await()).thenReturn(testData)
+        `when`(interactorTest.getSearchingGifs(text)).thenReturn(result)
+        val viewModel = MainViewModel(interactorTest)
 
-        val testData = listOf(Gif("test preview url", "test url"))
-        Mockito.`when`(interactorTest.getSearchingGifs(text).await()).thenReturn((testData))
+        //Mockito.verify(interactorTest, Mockito.times(1)).getSearchingGifs(text)
 
-        val expected = listOf(Gif("test preview url", "test url"))
-        val actual = interactorTest.getSearchingGifs(text).await()
+        // action
+        viewModel.onSearchTextChanged(text = text)
 
-        Assertions.assertNotNull(actual)
-        Assertions.assertEquals(expected, actual)
-
-/*        val expected = gifs
-        val actual = 2 + 2
-        Assertions.assertEquals(expected, actual)*/
+        // check
+        val actual = viewModel.searchText.value.toString()
+        Assertions.assertEquals(text, actual)
     }
 }
