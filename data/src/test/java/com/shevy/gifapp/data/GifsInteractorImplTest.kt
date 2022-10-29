e54f3c3c7ef44ce3300cad0e4fb5c6b0f40bce18
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-10-29 16:16:49 +0500

    Added test for getSearchingGifs() method

--- data/src/test/java/com/shevy/gifapp/data/GifsInteractorImplTest.kt
+++ data/src/test/java/com/shevy/gifapp/data/GifsInteractorImplTest.kt
@@ -13,7 +13,7 @@ import org.mockito.kotlin.mock
 internal class GifsInteractorImplTest {
 
     @Test
-    fun getTrendingGifsTest() = runBlocking {
+    fun `check that getTrendingGifs() method return right value`() = runBlocking {
         //setup
         val api = mock<GifsApi>()
         val original = Original("", "", "", "", "", "", "test url", "", "", "")
@@ -39,7 +39,7 @@ internal class GifsInteractorImplTest {
     }
 
     @Test
-    fun `check that the getSearchingGifs() method is called one`() {
+    fun `check that the getTrendingGifs() method is called one time`() {
         //setup
         val interactor = mock<GifsInteractorImpl>()
 
@@ -51,12 +51,42 @@ internal class GifsInteractorImplTest {
     }
 
     @Test
-    fun getSearchingGifsTest() {
+    fun `check that getSearchingGifs() method return right value`() = runBlocking {
         //setup
+        val api = mock<GifsApi>()
+        val original = Original("", "", "", "", "", "", "test url", "", "", "")
+        val original2 = Original("", "", "", "", "", "", "test url2", "", "", "")
+        val fixedWidthDownsampled = FixedWidthDownsampled("", "", "test preview url", "", "", "")
+        val fixedWidthDownsampled2 = FixedWidthDownsampled("", "", "test preview url2", "", "", "")
+        val images = Images(original, fixedWidthDownsampled)
+        val images2 = Images(original2, fixedWidthDownsampled2)
+        val response = GiphyDC(listOf(Gifs(images), Gifs(images2)))
+        val text = "Test"
+
+        `when`(api.getSearchingGifs(any(), any(), any(), any(), any(), any())).thenReturn(response)
+
+        val expected = listOf(
+            Gif("test preview url", "test url"),
+            Gif("test preview url2", "test url2")
+        )
 
         //action
+        val actual = GifsInteractorImpl(api).getSearchingGifs(text).await()
 
         //check
+        Assertions.assertEquals(expected, actual)
+    }
 
+    @Test
+    fun `check that the getSearchingGifs() method is called one time`() {
+        //setup
+        val interactor = mock<GifsInteractorImpl>()
+        val text = "Test"
+
+        //action
+        interactor.getSearchingGifs(text)
+
+        //check
+        Mockito.verify(interactor, Mockito.times(1)).getSearchingGifs(text)
     }
 }
